445ad25958061fadd2392a5d4ae5565140ab2319
Caleb Mabry <caleb.mabry.dev@gmail.com>
2023-10-27 11:36:58 -0400

    Clear up the json viewer

--- columns/jsonViewer/index.js
+++ columns/jsonViewer/index.js
@@ -1,22 +1,4 @@
-const privileges = [
-    'cellvalue',
-]
-const decodeAttributeByName = (fromClass, name) => {
-    const encodedJSON = fromClass.getAttribute(name);
-    const decodedJSON = encodedJSON
-        ?.replace(/&quot;/g, '"')
-        ?.replace(/&#39;/g, "'");
-
-    try {
-        return JSON.stringify(JSON.parse(decodedJSON))
-    } catch (e) {
-        return JSON.stringify({})
-    }
-}
-const STOP_EDITING_AND_CLOSE_EDITOR = "onstopedit"
-const UPDATE_CELL = "updatecell"
-
-var templateCell_$PLUGIN_ID = document.createElement('template')
+const templateCell_$PLUGIN_ID = document.createElement("template");
 templateCell_$PLUGIN_ID.innerHTML = `
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -65,9 +47,9 @@ templateCell_$PLUGIN_ID.innerHTML = `
     <input type="text" id="jsonValue" placeholder="Enter JSON...">
     <button id="view-json">{;}</button>
 </div>
-`
+`;
 
-var templateEditor_$PLUGIN_ID = document.createElement('template')
+const templateEditor_$PLUGIN_ID = document.createElement("template");
 templateEditor_$PLUGIN_ID.innerHTML = `
 <style>
     #container {
@@ -87,209 +69,253 @@ templateEditor_$PLUGIN_ID.innerHTML = `
         id="jsonEditor"
     ></textarea>
 </div>
-`
+`;
 
 // No Configuration needed
 class OuterbasePluginConfig_$PLUGIN_ID {
-    constructor(object) {
-    }
+  constructor(object) {}
 }
 
-
-
 class OuterbasePluginCell_$PLUGIN_ID extends HTMLElement {
-    static get observedAttributes() {
-        return privileges
+  static get observedAttributes() {
+    return ["cellvalue"];
+  }
+
+  config = new OuterbasePluginConfig_$PLUGIN_ID({});
+
+  constructor() {
+    super();
+
+    this.shadow = this.attachShadow({ mode: "open" });
+
+    this.shadow.appendChild(templateCell_$PLUGIN_ID.content.cloneNode(true));
+  }
+  decodeAttributeByName = (fromClass, name) => {
+    const encodedJSON = fromClass.getAttribute(name);
+    const decodedJSON = encodedJSON
+      ?.replace(/&quot;/g, '"')
+      ?.replace(/&#39;/g, "'");
+
+    try {
+      const decodedAttribute = JSON.stringify(JSON.parse(decodedJSON.replace( /\n/g, '' )));
+      return decodedAttribute;
+    } catch (e) {
+      return JSON.stringify({});
     }
+  };
 
-    config = new OuterbasePluginConfig_$PLUGIN_ID({})
+  // This function is called when the UI is made available into the DOM. Put any
+  // logic that you want to run when the element is first stood up here, such as
+  // event listeners, default values to display, etc.
+  connectedCallback() {
+    // Parse the configuration object from the `configuration` attribute
+    // and store it in the `config` property.
+    this.config = new OuterbasePluginConfig_$PLUGIN_ID(
+      JSON.parse(this.getAttribute("configuration"))
+    );
 
-    constructor() {
-        super()
+    let cellValue;
+    cellValue = JSON.stringify(
+      JSON.parse(this.decodeAttributeByName(this, "cellvalue")),
+      undefined,
+      2
+    );
+    const cell = this.shadow.getElementById("jsonValue");
+    cell.value = cellValue;
 
-        this.shadow = this.attachShadow({ mode: 'open' })
+    var jsonValue = this.shadow.getElementById("jsonValue");
+    var viewImageButton = this.shadow.getElementById("view-json");
 
-        this.shadow.appendChild(templateCell_$PLUGIN_ID.content.cloneNode(true))
-    }
+    jsonValue.addEventListener("focus", () => {
+      this.callCustomEvent({
+        action: "onstopedit",
+        value: true,
+      });
+    });
 
-    // This function is called when the UI is made available into the DOM. Put any
-    // logic that you want to run when the element is first stood up here, such as
-    // event listeners, default values to display, etc.
-    connectedCallback() {
-        // Parse the configuration object from the `configuration` attribute
-        // and store it in the `config` property.
-        this.config = new OuterbasePluginConfig_$PLUGIN_ID(
-            JSON.parse(this.getAttribute('configuration'))
-        )
-        console.log('The cell value', this.getAttribute('cellvalue'))
-        let cellValue;
-        cellValue = decodeAttributeByName(this, 'cellvalue')
-        const cell = this.shadow.getElementById('jsonValue')
-        cell.value = cellValue
+    jsonValue.addEventListener("blur", () => {
+      let jsonCellValue;
+      try {
+        jsonCellValue = JSON.stringify(
+          JSON.parse(jsonValue.value),
+          undefined,
+          2
+        );
+      } catch (e) {
+        jsonCellValue = JSON.stringify({});
+      }
+      this.callCustomEvent({
+        action: "updatecell",
+        value: jsonCellValue.replace( /\n/g, '' ),
+      });
 
-        var jsonValue = this.shadow.getElementById("jsonValue");
-        var viewImageButton = this.shadow.getElementById("view-json");
+      this.callCustomEvent({
+        action: "onstopedit",
+        value: true,
+      });
+    });
 
-        jsonValue.addEventListener("focus", () => {
-            this.callCustomEvent({
-                action: STOP_EDITING_AND_CLOSE_EDITOR,
-                value: true
-            })
-        });
+    jsonValue.addEventListener("keydown", (e) => {
+      if (e.code == "Enter" && e.shiftKey) {
+        return;
+      }
+      if (e.code != "Enter") {
+        return;
+      }
+      let jsonCellValue;
+      try {
+        jsonCellValue = JSON.stringify(
+          JSON.parse(jsonValue.value),
+          undefined,
+          2
+        );
+      } catch (e) {
+        jsonCellValue = JSON.stringify({});
+      }
+      this.callCustomEvent({
+        action: "updatecell",
+        value: jsonCellValue.replace( /\n/g, '' ),
+      });
 
-        jsonValue.addEventListener("blur", () => {
-            let jsonCellValue;
-            try {
-                jsonCellValue = JSON.stringify(JSON.parse(jsonValue.value), undefined, 2)
-            } catch (e) {
-                jsonCellValue = JSON.stringify({})
-            }
-            this.callCustomEvent({
-                action: UPDATE_CELL,
-                value: jsonCellValue
-            })
+      this.callCustomEvent({
+        action: "onstopedit",
+        value: true,
+      });
+      jsonValue.blur();
+    });
+    viewImageButton.addEventListener("click", () => {
+      this.callCustomEvent({
+        action: "onedit",
+        value: true,
+      });
+    });
+  }
 
-            this.callCustomEvent({
-                action: STOP_EDITING_AND_CLOSE_EDITOR,
-                value: true
-            })
-        });
-
-        jsonValue.addEventListener("keydown", (e) => {
-            if (e.code == "Enter" && e.shiftKey) {
-                return
-            }
-            if (e.code != "Enter") {
-                return
-            }
-            let jsonCellValue;
-            try {
-                jsonCellValue = JSON.stringify(JSON.parse(jsonValue.value), undefined, 2)
-            } catch (e) {
-                jsonCellValue = JSON.stringify({})
-            }
-            this.callCustomEvent({
-                action: UPDATE_CELL,
-                value: jsonCellValue
-            })
-
-            this.callCustomEvent({
-                action: STOP_EDITING_AND_CLOSE_EDITOR,
-                value: true
-            })
-            jsonValue.blur()
-        });
-        viewImageButton.addEventListener("click", () => {
-            console.log('Calling onedit')
-            this.callCustomEvent({
-                action: 'onedit',
-                value: true
-            })
-        });
-
-    }
-
-    callCustomEvent(data) {
-        const event = createCustomEvent(data)
-        this.dispatchEvent(event);
-    }
+  callCustomEvent(data) {
+    const event = createCustomEvent(data);
+    this.dispatchEvent(event);
+  }
 }
 
 class OuterbasePluginEditor_$PLUGIN_ID extends HTMLElement {
-    static get observedAttributes() {
-        return privileges
+  static get observedAttributes() {
+    return ["cellvalue"];
+  }
+
+  constructor() {
+    super();
+
+    // The shadow DOM is a separate DOM tree that is attached to the element.
+    // This allows us to encapsulate our styles and markup. It also prevents
+    // styles from the parent page from leaking into our plugin.
+    this.shadow = this.attachShadow({ mode: "open" });
+
+    this.shadow.appendChild(templateEditor_$PLUGIN_ID.content.cloneNode(true));
+
+    // Parse the configuration object from the `configuration` attribute
+    // and store it in the `config` property.
+    this.config = new OuterbasePluginConfig_$PLUGIN_ID(
+      JSON.parse(this.getAttribute("configuration"))
+    );
+  }
+  decodeAttributeByName = (fromClass, name) => {
+    const encodedJSON = fromClass.getAttribute(name);
+    const decodedJSON = encodedJSON
+      ?.replace(/&quot;/g, '"')
+      ?.replace(/&#39;/g, "'");
+
+    try {
+      return JSON.stringify(JSON.parse(decodedJSON));
+    } catch (e) {
+      return JSON.stringify({});
     }
+  };
 
-    constructor() {
-        super()
+  // This function is called when the UI is made available into the DOM. Put any
+  // logic that you want to run when the element is first stood up here, such as
+  // event listeners, default values to display, etc.
+  connectedCallback() {
+    const cellValue = this.decodeAttributeByName(this, "cellvalue");
 
-        // The shadow DOM is a separate DOM tree that is attached to the element.
-        // This allows us to encapsulate our styles and markup. It also prevents
-        // styles from the parent page from leaking into our plugin.
-        this.shadow = this.attachShadow({ mode: 'open' })
+    const jsonEditor = this.shadow.getElementById("jsonEditor");
+    jsonEditor.innerHTML = JSON.stringify(JSON.parse(cellValue), undefined, 2);
 
-        this.shadow.appendChild(templateEditor_$PLUGIN_ID.content.cloneNode(true))
+    jsonEditor.addEventListener("blur", (ev) => {
+      let cellEditorValue;
+      try {
+        cellEditorValue = JSON.stringify(
+          JSON.parse(jsonEditor.value),
+          undefined,
+          2
+        );
+      } catch (e) {
+        cellEditorValue = JSON.stringify({});
+      }
 
-        // Parse the configuration object from the `configuration` attribute
-        // and store it in the `config` property.
-        this.config = new OuterbasePluginConfig_$PLUGIN_ID(
-            JSON.parse(this.getAttribute('configuration'))
-        )
-    }
+      this.callCustomEvent({
+        action: "updatecell",
+        value: cellEditorValue.replace( /\n/g, '' ),
+      });
 
-    // This function is called when the UI is made available into the DOM. Put any
-    // logic that you want to run when the element is first stood up here, such as
-    // event listeners, default values to display, etc.
-    connectedCallback() {
+      this.callCustomEvent({
+        action: "onstopedit",
+        value: true,
+      });
+    });
 
-        let cellValue
-        cellValue = decodeAttributeByName(this, 'cellvalue')
+    jsonEditor.addEventListener("keydown", (e) => {
+      if (e.code == "Enter" && e.shiftKey) {
+        return;
+      }
+      if (e.code != "Enter") {
+        return;
+      }
+      e.preventDefault();
 
-        const jsonEditor = this.shadow.getElementById("jsonEditor");
-        console.log('Json editor value', cellValue)
-        jsonEditor.innerHTML = cellValue
+      let cellEditorValue;
+      try {
+        cellEditorValue = JSON.stringify(
+          JSON.parse(jsonEditor.value.replace( /\n/g, '' )),
+          undefined,
+          2
+        );
+      } catch (e) {
+        cellEditorValue = JSON.stringify({});
+      }
 
-        jsonEditor.addEventListener('blur', (ev) => {
-            let cellEditorValue
-            try {
-                cellEditorValue = JSON.stringify(JSON.parse(jsonEditor.value), undefined, 2)
-            } catch (e) {
-                cellEditorValue = JSON.stringify({})
-            }
-            console.log('Json editor update', cellEditorValue)
+      this.callCustomEvent({
+        action: "updatecell",
+        value: cellEditorValue.replace( /\n/g, '' ),
+      });
 
-            this.callCustomEvent({
-                action: UPDATE_CELL,
-                value: cellEditorValue
-            })
-
-            this.callCustomEvent({
-                action: STOP_EDITING_AND_CLOSE_EDITOR,
-                value: true
-            })
-        })
-
-        jsonEditor.addEventListener('keydown', (e) => {
-            if (e.code == "Enter" && e.shiftKey) {
-                return
-            }
-            if (e.code != "Enter") {
-                return
-            }
-            let cellEditorValue
-            try {
-                cellEditorValue = JSON.stringify(JSON.parse(jsonEditor.value), undefined, 2)
-            } catch (e) {
-                cellEditorValue = JSON.stringify({})
-            }
-
-
-            this.callCustomEvent({
-                action: UPDATE_CELL,
-                value: cellEditorValue
-            })
-
-            this.callCustomEvent({
-                action: STOP_EDITING_AND_CLOSE_EDITOR,
-                value: true
-            })
-        })
-    }
-    callCustomEvent(data) {
-        const event = createCustomEvent(data)
-        this.dispatchEvent(event);
-    }
+      this.callCustomEvent({
+        action: "onstopedit",
+        value: true,
+      });
+    });
+  }
+  callCustomEvent(data) {
+    const event = createCustomEvent(data);
+    this.dispatchEvent(event);
+  }
 }
 
-const createCustomEvent = (data) => new CustomEvent('custom-change', {
+const createCustomEvent = (data) =>
+  new CustomEvent("custom-change", {
     detail: data,
-    bubbles: true,  // If you want the event to bubble up through the DOM
-    composed: true  // Allows the event to pass through shadow DOM boundaries
-});
-
+    bubbles: true, // If you want the event to bubble up through the DOM
+    composed: true, // Allows the event to pass through shadow DOM boundaries
+  });
 
 // DO NOT change the name of this variable or the classes defined in this file.
 // Changing the name of this variable will cause your plugin to not work properly
 // when installed in Outerbase.
-window.customElements.define('outerbase-plugin-cell-$PLUGIN_ID', OuterbasePluginCell_$PLUGIN_ID)
-window.customElements.define('outerbase-plugin-editor-$PLUGIN_ID', OuterbasePluginEditor_$PLUGIN_ID)
+window.customElements.define(
+  "outerbase-plugin-cell-$PLUGIN_ID",
+  OuterbasePluginCell_$PLUGIN_ID
+);
+window.customElements.define(
+  "outerbase-plugin-editor-$PLUGIN_ID",
+  OuterbasePluginEditor_$PLUGIN_ID
+);
+
